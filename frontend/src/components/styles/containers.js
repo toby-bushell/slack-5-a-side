d8789808a017c37999b319f6c5fd0c379d55f908
Toby Bushell <toby.bushell@manifesto.co.uk>
2019-01-06 17:04:26 +0000

    moved backend back into subfolder

--- frontend/src/components/styles/containers.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  max-width: 100%;
-  width: 600px;
-  margin: 0 auto;
-`;
--- frontend/src/utils/format-time.js
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-
-export const formatToDay = time => {
-  return moment(time).format('dddd, Do, MMMM YYYY'); // "Sunday, February 14th 2010, 3:25:50 pm"
-};
