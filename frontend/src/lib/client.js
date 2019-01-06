b15749aebed9074756396bf6eea5356decb88358
Toby Bushell <toby.bushell@manifesto.co.uk>
2019-01-06 18:08:37 +0000

    log

--- frontend/src/lib/client.js
+++ frontend/src/lib/client.js
@@ -1,6 +1,8 @@
 import ApolloClient from 'apollo-boost';
 import { endpoint, prodEndpoint } from '../config';
 
+console.log('\x1b[31m', 'process.env.', process.env, '\x1b[0m');
+
 export const client = new ApolloClient({
   uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
   request: operation => {
