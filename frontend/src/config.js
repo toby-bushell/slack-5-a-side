1e69a0cac5e775df9ac173f366215675b510f134
Toby Bushell <toby.bushell@manifesto.co.uk>
2019-01-06 17:49:18 +0000

    configgggg prod

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4000`;
+export const prodEndpoint = `https://slack-5-aside.herokuapp.com/`;
